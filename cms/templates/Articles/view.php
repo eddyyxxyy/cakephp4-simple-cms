a73e15885281bb0e90abf7396d580f96d1439e04
Edson Pimenta <edson.tibo@gmail.com>
2023-02-07 14:58:24 -0300

    Feat: implements better tagging on views

--- cms/templates/Articles/view.php
+++ cms/templates/Articles/view.php
@@ -1,4 +1,5 @@
 <h1><?php echo h($article->title); ?></h1>
 <p><?php echo h($article->body); ?></p>
+<p><b>Tags: </b><?php echo h($article->tag_string); ?></p>
 <p><small>Created: <?php echo $article->created->format(DATE_RFC850); ?></small></p>
 <p><?php echo $this->Html->link('Edit', ['action' => 'edit', $article->slug]); ?></p>
